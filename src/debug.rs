dfac5b2604a9c3226fa9c66ef992b432d93d9ba0
btanno <149578422+btanno@users.noreply.github.com>
2024-06-07 09:51:06 +0900

    register_output_debug_string_handlersを実装 (#9)

--- src/debug.rs
+++ src/debug.rs
@@ -1,3 +1,4 @@
+use std::sync::{Mutex, OnceLock};
 use windows::core::{PCSTR, PCWSTR};
 use windows::Win32::Foundation::{DBG_PRINTEXCEPTION_C, DBG_PRINTEXCEPTION_WIDE_C};
 use windows::Win32::Graphics::Direct3D12::*;
@@ -15,30 +16,56 @@ pub fn enable_debug_layer() -> windows::core::Result<()> {
     Ok(())
 }
 
-pub fn output_debug_string_to_stderr() {
-    unsafe extern "system" fn handler(info: *mut EXCEPTION_POINTERS) -> i32 {
+static DBG_HANDLERS: OnceLock<Mutex<Vec<Box<(dyn Fn(&str) + Send + Sync + 'static)>>>> =
+    OnceLock::new();
+
+fn call_dbg_handlers(msg: &str) {
+    let Some(handlers) = DBG_HANDLERS.get().and_then(|handlers| handlers.lock().ok()) else {
+        return;
+    };
+    for handler in handlers.iter() {
+        handler(msg);
+    }
+}
+
+pub fn register_output_debug_string_handler(f: impl Fn(&str) + Send + Sync + 'static) {
+    unsafe extern "system" fn handler_proc(info: *mut EXCEPTION_POINTERS) -> i32 {
         let info = info.as_ref().unwrap();
         let record = info.ExceptionRecord.as_ref().unwrap();
         match record.ExceptionCode {
             DBG_PRINTEXCEPTION_C => {
                 let psz = PCSTR(record.ExceptionInformation[1] as *const u8);
                 if let Ok(msg) = psz.to_string() {
-                    eprintln!("{msg}");
+                    call_dbg_handlers(&msg);
                 }
                 EXCEPTION_CONTINUE_EXECUTION
             }
             DBG_PRINTEXCEPTION_WIDE_C => {
                 let psz = PCWSTR(record.ExceptionInformation[1] as *const u16);
                 if let Ok(msg) = psz.to_string() {
-                    eprintln!("{msg}");
+                    call_dbg_handlers(&msg);
                 }
                 EXCEPTION_CONTINUE_EXECUTION
             }
             _ => EXCEPTION_CONTINUE_SEARCH,
         }
     }
+    let handlers = DBG_HANDLERS.get_or_init(|| {
+        unsafe {
+            AddVectoredExceptionHandler(1, Some(handler_proc));
+        }
+        Mutex::new(vec![])
+    });
+    let mut handlers = handlers.lock().unwrap();
+    handlers.push(Box::new(f));
+}
 
-    unsafe {
-        AddVectoredExceptionHandler(1, Some(handler));
-    }
+#[inline]
+pub fn output_debug_string_to_stderr() {
+    static LOCK: OnceLock<()> = OnceLock::new();
+    LOCK.get_or_init(|| {
+        register_output_debug_string_handler(|msg| {
+            eprintln!("{msg}");
+        });
+    });
 }
